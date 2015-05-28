235611b9e2d23e8499d2ef7a98224b101519b11c
Robert Long <rlong@unicon.net>
2015-05-28 09:12:12 -0700

    SAK-43 refactored ckeditor services to reside in media-gallery instead of admin tool

--- ckeditor/kaltura/plugin.js
+++ ckeditor/kaltura/plugin.js
@@ -16,42 +16,37 @@
 //var CKEDITOR = CKEDITOR || {};
 var ckeditorId;
 (function() {
-  var pluginName = 'kaltura';
-  CKEDITOR.plugins.add(pluginName,
-      {
-          init: function(editor) {
+    var pluginName = 'kaltura';
+    CKEDITOR.plugins.add(pluginName,
+        {
+            init: function(editor) {
 
-              editor.addCommand(pluginName, {
-                  exec : function(editor) {
-                      ckeditorId = editor.name;
+                ckeditorId = editor.name;
+                var height = 480;
+                var width = 750;
+                CKEDITOR.dialog.addIframe(pluginName,
+                    'Kaltura',
+                    '/media-gallery-tool/ckeditor.htm?userid=' + parent.portal.user.id + '&siteid=' + parent.portal.siteId,
+                    width,
+                    height,
+                    function() {
+                        var ckDialog = CKEDITOR.dialog.getCurrent();
+                        document.getElementById(ckDialog.getButton('ok').domId).style.display='none';
+                        document.getElementById(ckDialog.getButton('cancel').domId).style.display='none';
+                    },
 
-                      var height = 480;
-                      var width = 750;
-                      CKEDITOR.dialog.addIframe('kaltura',
-                          'Kaltura',
-                          '/media-gallery-tool/ckeditor.htm?userid=' + parent.portal.user.id + '&siteid=' + parent.portal.siteId,
-                          width,
-                          height,
-                          function() {
-                              var ckDialog = CKEDITOR.dialog.getCurrent();
-                              document.getElementById(ckDialog.getButton('ok').domId).style.display='none';
-                              document.getElementById(ckDialog.getButton('cancel').domId).style.display='none';                              // iframe callback
-                          },
+                    {
+                        onOk : function() {}
+                    }
+                );
+                editor.addCommand(pluginName, new CKEDITOR.dialogCommand( 'kaltura' ) );
 
-                          {
-                              onOk : function() {}
-                          }
-                      );
-
-                      editor.addCommand( 'kaltura', new CKEDITOR.dialogCommand( 'kaltura' ) );
-                  }
-            });
-
-            editor.ui.addButton(pluginName, {
-                label: pluginName,
-                command: pluginName,
-                icon: this.path + 'images/kaltura.gif'
-            });
+                editor.ui.addButton(pluginName, {
+                    label: pluginName,
+                    command: pluginName,
+                    icon: this.path + 'images/kaltura.gif'
+                });
+            }
         }
     );
 
